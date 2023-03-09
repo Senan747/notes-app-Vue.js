ad6fdab5977c8eda184f0d26ab02e62bd75d8a9b
Senan747 <111648854+Senan747@users.noreply.github.com>
2023-03-09 08:37:37 +0400

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,23 @@
 <template>
   <main>
-    <!-- <div class="overlay">
+    
+    <div class="overlay" v-if="showModal">
       <div class="modal">
-        <textarea name="note" id="note" cols="30" rows="10"></textarea>
-        <button>Add Note</button>
-        <button class="close">Close</button>
+        <textarea v-model.trim="newNote" name="note" id="note" cols="30" rows="10"></textarea>
+        <p v-if="errorMessage">{{ errorMessage }}</p>
+        <button @click="addNote">Add Note</button>
+        <button class="close" @click="showModal = false">Close</button>
       </div>
-    </div> -->
+    </div>
     <div class="container">
       <header>
-        <h1>Notes {{ showModal }}</h1>
+        <h1>Notes</h1>
         <button @click="showModal = true">+</button>
-      </header>
+      </header> 
       <div class="cards-container">
-        <div class="card">
-          <p class="main-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nisi reiciendis non! Pariatur, quaerat ab!</p>
-          <p class="date">03/03/2004</p>
-        </div>
-        <div class="card">
-          <p class="main-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore nisi reiciendis non! Pariatur, quaerat ab!</p>
-          <p class="date">03/03/2004</p>
+        <div v-for="note in notes" :key="note.id" class="card" :style="{ backgroundColor: note.backgroundColor }">
+          <p class="main-text">{{ note.text }}</p>
+          <p class="date">{{ note.date.toLocaleDateString("en-US") }}</p>
         </div>
       </div>
     </div>
@@ -30,6 +28,29 @@
   import {ref} from "vue";
 
   const showModal = ref(false)
+  const newNote = ref("")
+  const errorMessage = ref("")
+  const notes = ref([])
+
+  function getRandomColor(){
+    return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
+    
+  }
+
+  const addNote = () => {
+    if(newNote.value.length < 9){
+      return errorMessage.value = "Note needs to be ten characters"
+    }
+    notes.value.push({
+      id: Math.floor(Math.random() * 1000000),
+      text: newNote.value,
+      date: new Date(),
+      backgroundColor: getRandomColor() 
+    });
+    showModal.value = false;
+    newNote.value = "";
+    errorMessage.value = "";
+  }
 
 </script>
 <style scoped>
@@ -123,5 +144,9 @@
   .modal .close {
     background-color: red;
     margin-top: 7px;
+  }
+
+  .modal p{
+    color: red;
   }
 </style>
